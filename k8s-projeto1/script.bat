79887a8de2b86f84db39ebae9029013b451bc483
Artur Issamu Takata <Aitakata@users.noreply.github.com>
2022-10-15 00:02:27 -0300

    Update script.bat

--- k8s-projeto1/script.bat
+++ k8s-projeto1/script.bat
@@ -7,3 +7,10 @@ echo "Realizando o push das imagens..."
 
 docker push aitakata/projeto-backend:1.0
 docker push aitakata/projeto-database:1.0
+
+echo "Criando serviços no cluster k8s..."
+
+kubectl apply -f ./services.yml
+
+echo "Realizando o deployment..."
+kubectl apply -f ./deployment.yml
